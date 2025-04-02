80f86ceaed8e1e6058c90d6d11bd9dc6b0f1d85e
Koval Sviatoslav <randomercook231@gmail.com>
2025-04-02 17:34:35 +0300

    здійснено початкове налаштування проєкту, встановлено бібліотеки, додано шрифти, поміняно favicon, додано змінні кольорів

---
+++ src/assets/fonts.ts
@@ -0,0 +1,16 @@
+import { Montserrat, Open_Sans, Roboto } from "next/font/google";
+
+export const montserrat = Montserrat({
+   subsets: ['latin'],
+   weight: ['400']
+});
+
+export const open_sans = Open_Sans({
+   subsets: ['latin'],
+   weight: ['400']
+});
+
+export const roboto = Roboto({
+   subsets: ['latin'],
+   weight: ['400']
+});
