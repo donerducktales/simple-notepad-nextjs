d47367b124a90d70d671fe5f0d8b389c39a3e3a9
Koval Sviatoslav <randomercook231@gmail.com>
2025-05-19 23:58:49 +0300

    встановив mongodb, створив db.ts для mongodb client, вивів дані замість notes.ts в NoteWrapper.tsx

--- src/components/NoteCard/NoteWrapper.tsx
+++ src/components/NoteCard/NoteWrapper.tsx
@@ -1,12 +1,38 @@
-import { notes } from "@/assets/notes";
+// import { notes } from "@/assets/notes";
+import { ObjectId, WithId } from "mongodb";
+import { client } from "@/lib/db";
 import NotePreview from "./NotePreview";
 
-export default function NoteWrapper() {
+interface Notes {
+   id: ObjectId;
+   title: string;
+   description: string;
+}
+
+export default async function NoteWrapper() {
+   async function getNotes() {
+      try {
+         const myClient = await client.connect();
+         const myDb = myClient.db('notes')
+         const myData = await myDb
+            .collection('note')
+            .find()
+            .toArray()
+
+         return myData;
+      } catch (error) {
+         console.error(error);
+         return [];
+      }
+   }
+
+   const notes = await getNotes() as WithId<Notes>[];
+
    return (
       <div className={`w-full flex flex-col items-center gap-3 mt-7 mb-4 ${'noteWrapper'}`}>
          {notes.map((el) => 
-            <NotePreview 
-               key={el.id}
+            <NotePreview
+               key={el._id.toString()}
                title={el.title}
                description={el.description}
             />
