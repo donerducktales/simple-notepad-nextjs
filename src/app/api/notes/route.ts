7bcb947ed95d5951ff8273825293bc130ec65043
Koval Sviatoslav <randomercook231@gmail.com>
2025-05-24 20:02:43 +0300

    додав await в route.ts(попередній коміт проблеми не вирішив)

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -4,7 +4,7 @@ export async function POST(request: Request) {
    const body = await request.json()
    const {title, description}: {title: string, description: string} = body;
 
-   const addData = addNote({title, description})
+   const addData = await addNote({title, description})
 
    return new Response(JSON.stringify(addData))
 }
