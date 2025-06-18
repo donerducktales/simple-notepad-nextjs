d90f9417f7ea271a559b40b7a00fd0357cab117a
Koval Sviatoslav <randomercook231@gmail.com>
2025-06-18 12:19:28 +0300

    Переправив в welcome щоб текст був по центру, і поміняв посилання на /home(логін і реєстрацію реалізую в майбутньому). Переробив filterNotes, щоб категорії були з бази даних, ну і зробив роут для цього(фільтрацію також зроблю в майбутньому)

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 export default function Welcome() {
    return (
       <section className={`flex flex-col justify-center items-center gap-4 w-full min-h-[100vh] mx-4 text-white ${'welcomePage'} ${poppins.className}`}>
-         <h1 className={`text-2xl font-medium ${'welcomePageHeader'}`}>
+         <h1 className={`text-2xl font-medium text-center ${'welcomePageHeader'}`}>
             <span className="font-extralight">Welcome to </span>
             SimpleNotepad
          </h1>
-         <Link href={'/login'}>
+         <Link href={'/home'}>
             <button className={`flex flex-row gap-4 w-72 h-12 rounded-xl items-center justify-center bg-primaryBlue font-medium ${'welcomePageButton'}`}>
                <PencilIcon className="text-white w-5"/>
                New Note
