cbc95244327163efaa52ce29a284220f17f5b529
Koval Sviatoslav <randomercook231@gmail.com>
2025-09-09 21:17:20 +0300

    зробив дріб'язкові доробки, deleteHandleClick.ts обгорнув в trycatch, в CreatePostForm та EditNoteForm зробив перевірку, щоб поля не були пусті, буде вилазити алерт, якщо нічого не лишити, createCategory додав mt в інпуті

--- src/components/DeleteNote/deleteHandleClick.ts
+++ src/components/DeleteNote/deleteHandleClick.ts
@@ -11,40 +11,43 @@ export async function handleClickDelete({
   type: string;
   dispatch: AppDispatch;
 }) {
-  const deleteNoteRes = await fetch("api/notes", {
-    body: JSON.stringify({ _id: _id }),
-    method: "DELETE",
-    cache: "no-store",
-  });
-
-  if (!deleteNoteRes.ok) {
-    console.error("Помилка при видаленні нотатки");
-    return;
-  }
-
-  
-  const countCategoriesRes = await fetch(`/api/count?type=${type}`, {
-    method: "GET",
-  });
-
-  const parsedCountCategories = await countCategoriesRes.json();
-
-  if (parsedCountCategories === 0) {
-    const deleteCategoryRes = await fetch("/api/categories", {
+  try {
+    const deleteNoteRes = await fetch("api/notes", {
+      body: JSON.stringify({ _id: _id }),
       method: "DELETE",
-      body: JSON.stringify({ type }),
-      headers: {
-        "Content-Type": "application/json",
-      },
+      cache: "no-store",
     });
 
-    if (!deleteCategoryRes.ok) {
-      console.error("Помилка при видаленні категорії");
+    if (!deleteNoteRes.ok) {
+      console.error("Помилка при видаленні нотатки");
+      return;
     }
-  }
 
-  dispatch(setTitle("Hello!"));
-  dispatch(setDescription("Here will be your notes"));
-  mutate("/api/notes");
-  mutate("/api/categories");
+    const countCategoriesRes = await fetch(`/api/count?type=${type}`, {
+      method: "GET",
+    });
+
+    const parsedCountCategories = await countCategoriesRes.json();
+
+    if (parsedCountCategories === 0) {
+      const deleteCategoryRes = await fetch("/api/categories", {
+        method: "DELETE",
+        body: JSON.stringify({ type }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!deleteCategoryRes.ok) {
+        console.error("Помилка при видаленні категорії");
+      }
+    }
+
+    dispatch(setTitle("Hello!"));
+    dispatch(setDescription("Here will be your notes"));
+    mutate("/api/notes");
+    mutate("/api/categories");
+  } catch (error) {
+    console.error("Delete error:", error);
+  }
 }
